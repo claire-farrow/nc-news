0ea8f153ba898d0e7724c4e119499e36314abe0b
Claire Farrow <clairefarrow.cp@gmail.com>
2022-03-09 12:41:20 +0000

    completed get comments by article id

---
+++ src/components/Comments.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import * as api from "../api";
+
+
+export default function Comments () {
+
+    const [comments, setComments] = useState([])
+
+    const {article_id} = useParams()
+
+    useEffect(() => {
+        api.fetchCommentsByArticleId(article_id).then((commentsFromApi) => {
+            setComments(commentsFromApi)
+            console.log(commentsFromApi)
+        })
+    }, [article_id])
+
+    return (
+        <section className="article">
+           <ul>
+            {comments.map((comment) => {
+                return (
+                    <article key={comment.comment_id} className="article-card">
+                        <p>{comment.article_id}</p>
+                        <p>{comment.comment_id}</p>
+                        <p>{comment.author}</p>
+                        <p>{comment.body}</p>
+                        <p>{comment.votes}</p>
+                        <p>{comment.create_at}</p>
+                    </article>              
+                )
+            })}
+            </ul>
+        </section>
+    )
+}
